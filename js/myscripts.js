c546cf666ffa2e2f880a39da95e3419f43f4e666
ste519 <qianzi@umich.edu>
2023-03-11 01:12:12 -0500

    change css and html

---
+++ js/myscripts.js
@@ -0,0 +1,8 @@
+function navControl() {
+    var x = document.getElementById("myNav");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
